93838c29181de449f905e844f233d9f662830f5d
daych12 <73504876+daych12@users.noreply.github.com>
2020-12-30 17:27:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+window.alert("Hello, world!");
+
+document.write('Hello, world!');
+
+var A = "This is a string";
+
+document.write(A);
+
+window.Alert(A);
+
+document.write("Dino told Ellis, \"Stop it, Ellis!\"<br>
+\"Eat mines!\" Ellis responded.");
+
+document.write("\"Be who tou are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\\""
++ "Dr. Seuss") ; 
