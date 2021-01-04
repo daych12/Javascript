50a9d20d71f18af59d00461ab61a2e06e9b340eb
daych12 <73504876+daych12@users.noreply.github.com>
2021-01-03 23:00:24 -0600

    changes

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -9,3 +9,18 @@ document.write(10 > 2 );
 console.log(2 + 2);
 document.write("10" + 5);
 document.write(10 == 10);
+
+X = 82;
+Y = "82";
+document.write(X === Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4);
+document.write(4 > 11 && 10 > 4);
+
+function not_Function()  {
+    document.getElementById("Not").innerHTML = ! (20 > 10);
+}
