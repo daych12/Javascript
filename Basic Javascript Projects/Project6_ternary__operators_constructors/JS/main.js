aa9a90441edad00e4152b5df4976ddf39bfd1ad4
daych12 <73504876+daych12@users.noreply.github.com>
2021-01-13 17:13:46 -0600

    changes

--- Basic Javascript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic Javascript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,3 +1,21 @@
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erick = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementsById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erick.Vehicle_Color + "-colored " + Erick.Vehicle_Model +
+    " manufactured in " + Erick.Vehicle_Year;
+}
+
+
+
+
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
