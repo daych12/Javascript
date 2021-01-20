5d566e1afeebf20c31a405ded38a6918e32b01e9
daych12 <73504876+daych12@users.noreply.github.com>
2021-01-20 16:53:13 -0600

    changes

--- Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
@@ -40,3 +40,6 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+$(function() {
+  $('body').removeClass('fade-out');
+});
